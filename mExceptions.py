684cef40df99a7a28c09e037f09cdcce1de82173
SkyLined <github@skylined.nl>
2021-10-27 14:03:36 +0200

    Update mExceptions.py

--- mExceptions.py
+++ mExceptions.py
@@ -1,2 +1,5 @@
 # Passdown from mHTTPConnection (and mHTTPProtocol by extension)
 from mHTTPConnection.mExceptions import *;
+from mHTTPConnection.mExceptions import acExceptions as acHTTPConnectionExceptions;
+
+acExceptions = acHTTPConnectionExceptions[:];
